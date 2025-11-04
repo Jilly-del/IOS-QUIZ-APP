bd53017978e22f6d99a54ac32f147fcf2cede1f9
MAC <jilboy72@gmail.com>
2025-11-04 11:31:33 -0800

    Initial commit

---
+++ Quiz-App/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question'.swift
+//  Quiz-App
+//
+//  Created by MAC on 04/11/2025.
+//
+
+import Foundation
+struct Question {
+    let question : String
+    let answer : String
+    init(q : String, a: String) {
+        question = q
+        answer = a
+    }
+}
